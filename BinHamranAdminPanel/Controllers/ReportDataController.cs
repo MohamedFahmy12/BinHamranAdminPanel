57ced2046ae8a50c261d2c88d75e625f44fab7b6
modyf <modyf@DESKTOP-3CROQE8>
2020-05-31 07:04:46 +0200

    31/05/2020

--- BinHamranAdminPanel/Controllers/ReportDataController.cs
+++ BinHamranAdminPanel/Controllers/ReportDataController.cs
@@ -10,12 +10,17 @@ using System.Data.Entity;
 using Microsoft.CodeAnalysis.Options;
 using Microsoft.Extensions.Options;
 using Microsoft.EntityFrameworkCore;
+using System.Runtime.Serialization.Json;
+using System.IO;
+using Stimulsoft.Report;
+using Newtonsoft.Json.Linq;
+using Stimulsoft.Report.Mvc;
 
 namespace BinHamranAdminPanel.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class ReportDataController : ControllerBase
+    public class ReportDataController : Controller
     {
         private ApplicationSettings appSettings;
         private IdentityAppContext context;
@@ -24,6 +29,77 @@ namespace BinHamranAdminPanel.Controllers
         {
             this.appSettings = appSettings.Value;
             this.context = context;
+        }
+        [HttpGet]
+        public string GetFile(int id)
+
+        {
+            StiReport report = new StiReport();
+            report.Dictionary.DataStore.Clear();
+            report.Load(Path.GetFullPath("SimpleList.mrt"));
+            report.Dictionary.Variables["id"].ValueObject = id;
+            string data = report.SaveToJsonString();
+
+            //string filePath = Path.GetFullPath("SimpleList.mrt");
+
+            //StreamReader rd = new StreamReader(filePath);
+            //string data = rd.ReadToEnd();
+            //rd.Close();
+
+            return data;
+        }
+
+
+        [HttpPost]
+        [Route("SaveFile")]
+        public string SaveFile([FromBody] DemoData jsonString)
+        {
+           
+           var reportName = jsonString.fileName;
+            try
+            {
+                string filePath = StiNetCoreHelper.MapPath(this,"C:/MohamedFahmy/BinHamranAdminPanelFront/src/reports/" +reportName+".mrt");
+                StreamWriter wr = new StreamWriter(filePath);
+                wr.Write(jsonString.data);
+                wr.Close();
+                return "Report Saved Successfully";
+            }
+            catch (Exception ex)
+            {
+                return ex.Message;
+            }
+        }
+
+
+        [HttpPost]
+        [Route("GetDataSource")]
+        public void GetDataSource()
+        {
+
+
+            try
+            {
+                var command = (CommandJson)new DataContractJsonSerializer(typeof(CommandJson)).ReadObject(HttpContext.Request.BodyReader.AsStream());
+                Result result = new Result();
+
+                if (command.Database == "MS SQL")
+                {
+                    result = MSSQLAdapter.Process(command);
+                }
+                //if (command.Database == "PostgreSQL") result = PostgreSQLAdapter.Process(command);
+
+                var serializer = new DataContractJsonSerializer(typeof(Result));
+
+                serializer.WriteObject(HttpContext.Response.BodyWriter.AsStream(), result);
+                HttpContext.Response.Headers.Add("Access-Control-Allow-Origin", "*");
+                HttpContext.Response.Headers.Add("Cache-Control", "no-cache");
+
+                HttpContext.Response.Body.Flush();
+                HttpContext.Response.Clear();
+            }
+            catch { }
+
+
         }
         [HttpGet]
         [Route("GetDbNames")]

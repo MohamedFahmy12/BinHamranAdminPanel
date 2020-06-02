aa187bacfbe361124b2376948da9e458052a74d2
MohamedFahmy12 <mohmedfahmyali@gmail.com>
2020-06-02 14:42:04 +0200

    02-06-2020

--- BinHamranAdminPanel/Controllers/ReportDataController.cs
+++ BinHamranAdminPanel/Controllers/ReportDataController.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using Microsoft.Data.SqlClient;
 using System.Linq;
@@ -18,155 +18,159 @@ using Stimulsoft.Report.Mvc;
 
 namespace BinHamranAdminPanel.Controllers
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class ReportDataController : Controller
+  [Route("api/[controller]")]
+  [ApiController]
+  public class ReportDataController : Controller
+  {
+    private ApplicationSettings appSettings;
+    private IdentityAppContext context;
+
+    public ReportDataController(IOptions<ApplicationSettings> appSettings, IdentityAppContext context)
     {
-        private ApplicationSettings appSettings;
-        private IdentityAppContext context;
-
-        public ReportDataController(IOptions<ApplicationSettings> appSettings, IdentityAppContext context)
-        {
-            this.appSettings = appSettings.Value;
-            this.context = context;
-        }
-        [HttpGet]
-        public string GetFile(int id)
-
-        {
-            StiReport report = new StiReport();
-            report.Dictionary.DataStore.Clear();
-            report.Load(Path.GetFullPath("SimpleList.mrt"));
-            report.Dictionary.Variables["id"].ValueObject = id;
-            string data = report.SaveToJsonString();
-
-            //string filePath = Path.GetFullPath("SimpleList.mrt");
-
-            //StreamReader rd = new StreamReader(filePath);
-            //string data = rd.ReadToEnd();
-            //rd.Close();
-
-            return data;
-        }
-
-
-        [HttpPost]
-        [Route("SaveFile")]
-        public string SaveFile([FromBody] DemoData jsonString)
-        {
-           
-           var reportName = jsonString.fileName;
-            try
-            {
-                string filePath = StiNetCoreHelper.MapPath(this,"C:/MohamedFahmy/BinHamranAdminPanelFront/src/reports/" +reportName+".mrt");
-                StreamWriter wr = new StreamWriter(filePath);
-                wr.Write(jsonString.data);
-                wr.Close();
-                return "Report Saved Successfully";
-            }
-            catch (Exception ex)
-            {
-                return ex.Message;
-            }
-        }
-
-
-        [HttpPost]
-        [Route("GetDataSource")]
-        public void GetDataSource()
-        {
-
-
-            try
-            {
-                var command = (CommandJson)new DataContractJsonSerializer(typeof(CommandJson)).ReadObject(HttpContext.Request.BodyReader.AsStream());
-                Result result = new Result();
-
-                if (command.Database == "MS SQL")
-                {
-                    result = MSSQLAdapter.Process(command);
-                }
-                //if (command.Database == "PostgreSQL") result = PostgreSQLAdapter.Process(command);
-
-                var serializer = new DataContractJsonSerializer(typeof(Result));
-
-                serializer.WriteObject(HttpContext.Response.BodyWriter.AsStream(), result);
-                HttpContext.Response.Headers.Add("Access-Control-Allow-Origin", "*");
-                HttpContext.Response.Headers.Add("Cache-Control", "no-cache");
-
-                HttpContext.Response.Body.Flush();
-                HttpContext.Response.Clear();
-            }
-            catch { }
-
-
-        }
-        [HttpGet]
-        [Route("GetDbNames")]
-        public List<DatabaseModel> GetDbNames()
-        {
-            var dbNames = context.DbNames.ToList();
-            var Dbs = new List<DatabaseModel>();
-            foreach (var item in dbNames)
-            {
-                var Db = new DatabaseModel();
-
-                  Db.DatabaseNameId = item.DatabaseNameId;
-                  Db.DbName = item.DbArabicName;
-                 Dbs.Add(Db);
-
-            }
-
-            //var Db = new DatabaseModel();
-            //var dbNames =  context.DbNames.ToList();
-            //foreach (var item in dbNames)
-            //{
-
-            //    Db.DatabaseNameId = item.DatabaseNameId;
-            //    Db.DbName = item.DbArabicName;
-            //    Dbs.Add(Db);
-            //};
-            return Dbs;
-
-        }
-        [HttpGet]
-        [Route("GetCompanyBranches")]
-        public IQueryable<BranchModel> GetCompanyBranches(string dbIds)
-        {
-
-            var parameter = new SqlParameter
-            {
-                ParameterName = "DatabaseID",
-                Value = dbIds
-            };
-            var Branches = context.Branchesm.FromSqlRaw("EXEC Get_CompanyBranches @DatabaseID", parameter);
-            return Branches;
-        }
-        [HttpGet]
-        [Route("GetEntries")]
-        public IQueryable<EntryModel> GetEntries(string dbIds)
-        {
-            var parameter = new SqlParameter
-            {
-                ParameterName = "DatabaseID",
-                Value = dbIds
-            };
-            var Entries = context.Entries.FromSqlRaw("EXEC Get_EntryType @DatabaseID", parameter);
-            return Entries;
-        }
-        [HttpGet]
-        [Route("GetAccounts")]
-        public IQueryable<AccountModel> GetAccounts(string dbIds)
-        {
-            var parameter = new SqlParameter
-            {
-                ParameterName = "DatabaseID",
-                Value = dbIds
-            };
-            var Accounts = context.Accounts.FromSqlRaw("EXEC Get_ACCOUNTS @DatabaseID", parameter);
-            return Accounts;
-        }
+      this.appSettings = appSettings.Value;
+      this.context = context;
     }
+    [HttpGet]
+    public string GetFile(int id)
+
+    {
+      StiReport report = new StiReport();
+      report.Dictionary.DataStore.Clear();
+      report.Load(Path.GetFullPath("SimpleList.mrt"));
+      report.Dictionary.Variables["id"].ValueObject = id;
+      string data = report.SaveToJsonString();
+
+      return data;
+    }
+    [HttpGet]
+    [Route("getReportForDesigner")]
+
+    public string getReportForDesigner(string reportName)
+    {
+      var path = StiNetCoreHelper.MapPath(this, "/Reports/" + reportName + ".mrt");
+      StreamReader rd = new StreamReader(path);
+      string data = rd.ReadToEnd();
+      rd.Close();
+      return data;
+    }
+
+    [HttpPost]
+    [Route("SaveFile")]
+    public string SaveFile([FromBody] DemoData jsonString)
+    {
+
+      var reportName = jsonString.fileName;
+      try
+      {
+        string filePath = StiNetCoreHelper.MapPath(this,"/Reports/" + reportName + ".mrt");
+        StreamWriter wr = new StreamWriter(filePath);
+        wr.Write(jsonString.data);
+        wr.Close();
+        return "Report Saved Successfully";
+      }
+      catch (Exception ex)
+      {
+        return ex.Message;
+      }
+    }
+
+
+    [HttpPost]
+    [Route("GetDataSource")]
+    public void GetDataSource()
+    {
+
+
+      try
+      {
+        var command = (CommandJson)new DataContractJsonSerializer(typeof(CommandJson)).ReadObject(HttpContext.Request.BodyReader.AsStream());
+        Result result = new Result();
+
+        if (command.Database == "MS SQL")
+        {
+          result = MSSQLAdapter.Process(command);
+        }
+        //if (command.Database == "PostgreSQL") result = PostgreSQLAdapter.Process(command);
+
+        var serializer = new DataContractJsonSerializer(typeof(Result));
+
+        serializer.WriteObject(HttpContext.Response.BodyWriter.AsStream(), result);
+        HttpContext.Response.Headers.Add("Access-Control-Allow-Origin", "*");
+        HttpContext.Response.Headers.Add("Cache-Control", "no-cache");
+
+        HttpContext.Response.Body.Flush();
+        HttpContext.Response.Clear();
+      }
+      catch { }
+
+
+    }
+    [HttpGet]
+    [Route("GetDbNames")]
+    public List<DatabaseModel> GetDbNames()
+    {
+      var dbNames = context.DbNames.ToList();
+      var Dbs = new List<DatabaseModel>();
+      foreach (var item in dbNames)
+      {
+        var Db = new DatabaseModel();
+
+        Db.DatabaseNameId = item.DatabaseNameId;
+        Db.DbName = item.DbArabicName;
+        Dbs.Add(Db);
+
+      }
+
+      //var Db = new DatabaseModel();
+      //var dbNames =  context.DbNames.ToList();
+      //foreach (var item in dbNames)
+      //{
+
+      //    Db.DatabaseNameId = item.DatabaseNameId;
+      //    Db.DbName = item.DbArabicName;
+      //    Dbs.Add(Db);
+      //};
+      return Dbs;
+
+    }
+    [HttpGet]
+    [Route("GetCompanyBranches")]
+    public IQueryable<BranchModel> GetCompanyBranches(string dbIds)
+    {
+
+      var parameter = new SqlParameter
+      {
+        ParameterName = "DatabaseID",
+        Value = dbIds
+      };
+      var Branches = context.Branchesm.FromSqlRaw("EXEC Get_CompanyBranches @DatabaseID", parameter);
+      return Branches;
+    }
+    [HttpGet]
+    [Route("GetEntries")]
+    public IQueryable<EntryModel> GetEntries(string dbIds)
+    {
+      var parameter = new SqlParameter
+      {
+        ParameterName = "DatabaseID",
+        Value = dbIds
+      };
+      var Entries = context.Entries.FromSqlRaw("EXEC Get_EntryType @DatabaseID", parameter);
+      return Entries;
+    }
+    [HttpGet]
+    [Route("GetAccounts")]
+    public IQueryable<AccountModel> GetAccounts(string dbIds)
+    {
+      var parameter = new SqlParameter
+      {
+        ParameterName = "DatabaseID",
+        Value = dbIds
+      };
+      var Accounts = context.Accounts.FromSqlRaw("EXEC Get_ACCOUNTS @DatabaseID", parameter);
+      return Accounts;
+    }
+  }
 
 
 }
--- BinHamranAdminPanel/Models/DemoData.cs
+++ BinHamranAdminPanel/Models/DemoData.cs
@@ -1,13 +1,14 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace BinHamranAdminPanel.Models
 {
-    public class DemoData
-    {
-        public string fileName { get; set; }
-        public string data { get; set; }
-    }
+  public class DemoData
+  {
+    public string fileName { get; set; }
+    public string data { get; set; }
+    public string currentlocation { get; set; }
+  }
 }
